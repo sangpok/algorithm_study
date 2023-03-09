3cd57cdf33a1f40ac4507637192e5bb0e924b932
sangpok <48979587+sangpok@users.noreply.github.com>
2023-03-09 15:24:15 +0900

    230307 최대공약수와 최소공배수 - 수정

--- 2609/solution.js
+++ 2609/solution.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
-const datas = fs.readFileSync(filePath).toString().trim().split(" ");
+const datas = fs.readFileSync(filePath).toString().trim().split(' ');
 
 function solution(numA, numB) {
-  const getGCD = (a, b) => !(a % b) ? b : gcd(b, a % b);
-  const getLCM = (a, b, gcd) = a * b / gcd;
-  
+  const getGCD = (a, b) => (!(a % b) ? b : getGCD(b, a % b));
+  const getLCM = (a, b, gcd) => (a * b) / gcd;
+
   const gcd = getGCD(numA, numB);
   const lcm = getLCM(numA, numB, gcd);
 
