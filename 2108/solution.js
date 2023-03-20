381dc24ae11b46ef536a9771b262494015025bb7
sangpok <48979587+sangpok@users.noreply.github.com>
2023-03-20 14:04:34 +0900

    230318 통계학

--- 2108/solution.js
+++ 2108/solution.js
@@ -3,59 +3,34 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/inp
 const [L, ...datas] = fs.readFileSync(filePath).toString().trim().split('\n');
 
 function solution(n, datas) {
-  let answers = new Map([
-    ['average', 0],
-    ['mid', 0],
-    ['mode', 0],
-    ['range', 0],
-  ]);
+  datas.sort((a, b) => a - b);
 
-  let sum = 0;
+  let mid = datas[(n - 1) / 2];
+  let [min, max] = [datas[0], datas[n - 1]];
 
-  let midIndex = (n - 1) / 2;
-
-  let min = Number.MAX_SAFE_INTEGER;
-  let max = Number.MIN_SAFE_INTEGER;
-
-  let modeMap = new Map();
+  const modeMap = new Map();
   let modeValue = Number.MIN_SAFE_INTEGER;
 
-  datas
-    .sort((a, b) => a - b)
-    .forEach((data, index) => {
-      sum += data;
+  let average = Number(
+    (
+      datas.reduce((a, b) => {
+        modeMap.set(b, (modeMap.get(b) || 0) + 1);
 
-      if (index === n - 1) {
-        const average = Number((sum / n).toFixed());
-        const isNegativeZero = Object.is(average, -0);
+        const modeB = modeMap.get(b);
+        modeValue = modeValue < modeB ? modeB : modeValue;
 
-        answers.set('average', isNegativeZero ? 0 : average);
-      }
+        return a + b;
+      }, 0) / n
+    ).toFixed()
+  );
 
-      if (index === midIndex) {
-        answers.set('mid', data);
-      }
+  const isNegativeZero = Object.is(average, -0);
+  average = isNegativeZero ? 0 : average;
 
-      if (min > data) {
-        min = data;
-      }
+  const modes = [...modeMap.entries()].filter(([_, value]) => value === modeValue);
+  let mode = modes.length > 1 ? modes[1][0] : modes[0][0];
 
-      if (max < data) {
-        max = data;
-      }
-
-      modeMap.set(data, (modeMap.get(data) || 0) + 1);
-      if (modeValue < modeMap.get(data)) {
-        modeValue = modeMap.get(data);
-      }
-    });
-
-  let modes = [...modeMap.entries()].filter(([_, value]) => value === modeValue);
-
-  answers.set('mode', modes.length > 1 ? modes[1][0] : modes[0][0]);
-  answers.set('range', max - min);
-
-  console.log([...answers.values()].join('\n'));
+  console.log([average, mid, mode, max - min].join('\n'));
 }
 
 solution(Number(L), datas.map(Number));
