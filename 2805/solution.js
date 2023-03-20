6a710983347f8c3d665a507cf31827a80b1c1c3a
sangpok <48979587+sangpok@users.noreply.github.com>
2023-03-20 17:42:47 +0900

    230319 설탕 배달

--- 2805/solution.js
+++ 2805/solution.js
@@ -1,32 +1,26 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
-const [L, ...datas] = fs.readFileSync(filePath).toString().trim().split('\n');
+const L = fs.readFileSync(filePath).toString().trim();
+// const [L, ...datas] = fs.readFileSync(filePath).toString().trim().split('\n');
 
-function solution([n, m], trees) {
-  let [rangeStart, rangeEnd] = [0, 1_000_000_000];
-  let answer = Number.MIN_SAFE_INTEGER;
+function solution(n) {
+  const fivePackCount = ~~(n / 5);
 
-  while (rangeStart <= rangeEnd) {
-    const midLength = ~~((rangeStart + rangeEnd) / 2);
+  if (!(n - fivePackCount * 5)) {
+    console.log(fivePackCount);
+    return;
+  }
 
-    let slicedTreeLength = 0;
-    for (let index = 0, treeCount = n; index < treeCount; index++) {
-      const currentTreeLength = trees[index];
+  for (let fivePack = fivePackCount; fivePack >= 0; fivePack--) {
+    const threePack = (n - fivePack * 5) / 3;
 
-      if (currentTreeLength > midLength) {
-        slicedTreeLength += currentTreeLength - midLength;
-      }
-    }
-
-    if (slicedTreeLength >= m) {
-      answer = answer < midLength ? midLength : answer;
-      rangeStart = midLength + 1;
-    } else {
-      rangeEnd = midLength - 1;
+    if (!(threePack - ~~threePack)) {
+      console.log(threePack + fivePack);
+      return;
     }
   }
 
-  console.log(answer);
+  console.log(-1);
 }
 
-solution(L.split(' ').map(Number), datas[0].split(' ').map(Number));
+solution(Number(L));
