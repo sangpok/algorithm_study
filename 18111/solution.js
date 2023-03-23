9fde05352f744a560f765c3b6bf48d471f4b2bb7
sangpok <48979587+sangpok@users.noreply.github.com>
2023-03-23 14:33:11 +0900

    230320 마인크래프트 / 수정

--- 18111/solution.js
+++ 18111/solution.js
@@ -3,22 +3,37 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/inp
 const [L, ...datas] = fs.readFileSync(filePath).toString().trim().split('\n');
 
 function solution([n, m, b], datas) {
+  const [mapWithoutSame, blockCountMap] = [new Set(), new Map()];
+
+  for (let dataIndex = 0, dataCount = n * m; dataIndex < dataCount; dataIndex++) {
+    const currentData = datas[dataIndex];
+
+    mapWithoutSame.add(currentData);
+    blockCountMap.set(currentData, (blockCountMap.get(currentData) || 0) + 1);
+  }
+
+  const blockHeights = [...mapWithoutSame.keys()];
+
   let minDuration = Number.MAX_SAFE_INTEGER;
   let answer = [minDuration, 0];
 
   for (let currentHeight = 0; currentHeight <= 256; currentHeight++) {
     let [blockToBeDelete, blockToBeFill] = [0, 0];
 
-    for (let dataIndex = 0, dataCount = n * m; dataIndex < dataCount; dataIndex++) {
-      const blockHeight = datas[dataIndex];
+    for (
+      let blockIndex = 0, blockCount = blockHeights.length;
+      blockIndex < blockCount;
+      blockIndex++
+    ) {
+      const blockHeight = blockHeights[blockIndex];
 
       if (blockHeight > currentHeight) {
-        blockToBeDelete += blockHeight - currentHeight;
+        blockToBeDelete += (blockHeight - currentHeight) * blockCountMap.get(blockHeight);
         continue;
       }
 
       if (blockHeight < currentHeight) {
-        blockToBeFill += currentHeight - blockHeight;
+        blockToBeFill += (currentHeight - blockHeight) * blockCountMap.get(blockHeight);
       }
     }
 
@@ -37,4 +52,4 @@ function solution([n, m, b], datas) {
   console.log(answer.join(' '));
 }
 
-solution(L.split(' ').map(Number), datas.map((data) => data.split(' ').map(Number)).flat());
+solution(L.split(' ').map(Number), [].concat(...datas.map((data) => data.split(' ').map(Number))));
